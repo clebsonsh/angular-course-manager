1188684aca9769a9f71ec5ca7eccaee6425fd678
Clebson Moura <clebson.sh@gmail.com>
2020-07-03 17:06:08 -0300

    Angular components

---
+++ src/app/pipe/replace.pipe.ts
@@ -0,0 +1,13 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'replace'
+})
+
+export class ReplacePipe implements PipeTransform {
+
+  transform(value: string, char: string, valueToRepalce) {
+    return value.replace(char, valueToRepalce);
+  }
+
+}
